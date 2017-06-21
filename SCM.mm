a10bd452d9808e9682d5c42fd8fdfe058498f7c7
Darshan Deshmukh <darshandeshmukh11@gmail.com>
2017-06-21 19:23:19 +0530

    Added Mindmap for SCM

---
+++ SCM.mm
@@ -0,0 +1,15 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1498051777980" ID="ID_63766585" MODIFIED="1498051811990" TEXT="SCM">
+<font NAME="Verdana" SIZE="12"/>
+<node CREATED="1498052653946" ID="ID_88499775" MODIFIED="1498052773453" POSITION="right" TEXT="SVN to Git migration">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1498052697595" ID="ID_1078094622" MODIFIED="1498052778004" POSITION="right" TEXT="TFS to Git migration">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+<node CREATED="1498052722920" ID="ID_1006333096" MODIFIED="1498052782856" POSITION="left" TEXT="Bitbucket Pull Request">
+<font NAME="Verdana" SIZE="12"/>
+</node>
+</node>
+</map>
